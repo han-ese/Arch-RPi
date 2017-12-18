63fc58e0f21fe73141d5b76d2b50a0722dd881bf
MvdS <han@vandersluys.nl>
2017-12-18 08:53:52 +0100

    Add han-ese-ops_run-at-shutdown.sh to repo

---
+++ shell/han-ese-ops_run-at-shutdown.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+##  Script to run at shutdown of a Raspberry Pi with a Sense HAT
+##  MvdD, 2017-10-14
+
+
+# Print a shutdown message on Sense HAT:
+/usr/bin/han-ese-ops_shutdown.py
+
